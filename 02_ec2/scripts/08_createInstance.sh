674923cc5edd7a3de1c62ab77f4e6efb82ca43f8
ilkayaktas <ilkayaktas@gmail.com>
2020-11-26 15:08:08 +0300

    Move object and synf folder ability are added for S3.

--- 02_ec2/scripts/08_createInstance.sh
+++ 02_ec2/scripts/08_createInstance.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
+# ubuntu ami-0a91cd140a1fc148a
+# amazon linux ami-0b0f4c27376f8aa79
 aws ec2 run-instances \
         --image-id ami-0a91cd140a1fc148a \
         --count 1 \
         --instance-type t2.micro \
         --key-name OpenfireKeyPair \
         --associate-public-ip-addres \
-        --security-group-ids sg-0d9a1875bfcb1cde7 \
-        --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=TheMachine}]'
+        --security-group-ids sg-0d9a1875bfcb1cde7s \
+        --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Openfire}]'
 
 
---
+++ 03_s3/scripts/07_moveObjectThroughBucket.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+aws s3 mv s3://my-bucket/file1.txt s3://my-new-bucket
