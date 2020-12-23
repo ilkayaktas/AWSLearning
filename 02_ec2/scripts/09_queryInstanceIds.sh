55e0c1ea155721c9357b779e4fe700f0c38343aa
ilkayaktas <ilkayaktas@gmail.com>
2020-12-23 08:31:40 +0300

    Api script and webserver are created for API Gateway experiment.

--- 02_ec2/scripts/09_queryInstanceIds.sh
+++ 02_ec2/scripts/09_queryInstanceIds.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId, State.Name]' --output text
+aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId, State.Name, Tags[?Key==`Name`].Value]' --output json
---
+++ 05_apigateway/webserver/webserver.js
@@ -0,0 +1,16 @@
+var http = require('http');
+var os = require('os');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+
+    if (req.url == "/users"){
+        res.write("Ahmet, Mehmet, Ayse");
+    }
+    else if(req.url == "/count"){
+        res.write("3");
+    } else{
+        res.write("No request!!! ");
+    }
+    res.end("\n"+os.hostname()+"\n");
+}).listen(8888);
